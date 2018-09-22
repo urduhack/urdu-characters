e5f78e2c8c09aa8fc197e93d343aeea0b8e022e3
Ikram Ali <mrikram1989@gmail.com>
2018-09-22 05:05:03 +0500

    [alphabet] Urdu alphabet updated.

--- urdu_alphabet.py
+++ urdu_alphabet.py
@@ -13,7 +13,7 @@ URDU_DIGITS = frozenset(""" ۰ ۱ ۲ ۳ ۴ ۵ ۶ ۷ ۸ ۹ """)
 URDU_ALPHABET_COMPLETE = frozenset("""
 
   آ ا ب پ ت ٹ ث ج چ ح خ د ڈ ذ ر ڑ ز ژ
- س ش ص ض ط ظ ع غ ف ق ك ک گ ل م ن ں و ؤ ہ ه ھ ء ی ے ‬
+ س ش ص ض ط ظ ع غ ف ق ک گ ل م ن ں و ؤ ہ ه ھ ء ی ے ‬
 
 """.split())
 
