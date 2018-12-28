5b1ed96d6a5466da8e1ce5f8323f754f8564766b
Ikram Ali <mrikram1989@gmail.com>
2018-12-28 11:40:17 +0500

    [character] bug fixed.

--- test_urdu_alphabet.py
+++ test_urdu_alphabet.py
@@ -1,8 +1,8 @@
 # coding: utf8
 """ Test cases """
 
-from urdu_alphabet import URDU_CHARACTERS, URDU_ALPHABET, URDU_DIGITS, URDU_PUNCTUATIONS, URDU_DIACRITICS
-from urdu_alphabet import URDU_CHARACTERS_UNICODE
+from urdu_alphabet import URDU_ALL_CHARACTERS, URDU_ALPHABETS, URDU_DIGITS, URDU_PUNCTUATIONS, URDU_DIACRITICS
+from urdu_alphabet import URDU_ALL_CHARACTERS_UNICODE
 import re
 
 urdu_unicode_range = re.compile("[^\u0600-\u06ff]+")
@@ -13,35 +13,35 @@ class TestUrduAlphabet(object):
 
     def test_urdu_alphabet(self):
         """ Test urdu_alphabet"""
-        for character in URDU_ALPHABET:
-            assert character in URDU_CHARACTERS
+        for character in URDU_ALPHABETS:
+            assert character in URDU_ALL_CHARACTERS
             assert len(urdu_unicode_range.findall(character)) == 0
 
     def test_urdu_digits(self):
         """ Test """
         for character in URDU_DIGITS:
-            assert character in URDU_CHARACTERS
+            assert character in URDU_ALL_CHARACTERS
             assert len(urdu_unicode_range.findall(character)) == 0
 
     def test_urdu_punctuation(self):
         """ Test """
         for character in URDU_PUNCTUATIONS:
-            assert character in URDU_CHARACTERS
+            assert character in URDU_ALL_CHARACTERS
             assert len(urdu_unicode_range.findall(character)) == 0
 
     def test_diacritics(self):
         """ Test """
         for character in URDU_DIACRITICS:
-            assert character in URDU_CHARACTERS
+            assert character in URDU_ALL_CHARACTERS
             assert len(urdu_unicode_range.findall(character)) == 0
 
     def test_unicode(self):
         """ Test """
-        for character in URDU_CHARACTERS:
+        for character in URDU_ALL_CHARACTERS:
             if character == "\u202c":
                 continue
-            assert character in URDU_CHARACTERS_UNICODE
+            assert character in URDU_ALL_CHARACTERS_UNICODE
 
-        for character, value in URDU_CHARACTERS_UNICODE.items():
-            assert character in URDU_CHARACTERS
-            assert value in URDU_CHARACTERS
+        for character, value in URDU_ALL_CHARACTERS_UNICODE.items():
+            assert character in URDU_ALL_CHARACTERS
+            assert value in URDU_ALL_CHARACTERS
