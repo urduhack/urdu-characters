abe9e56260dd3d34341def3f91c7d2b5712fcfae
Ikram Ali <mrikram1989@gmail.com>
2019-01-29 10:23:24 +0500

    [character] URDU_DIACRITICS set updated with new unicode characters.

--- test_urdu_characters.py
+++ test_urdu_characters.py
@@ -1,39 +1,39 @@
 # coding: utf8
 """ Test cases """
-
+import re
 from urdu_characters import URDU_ALL_CHARACTERS, URDU_ALPHABETS, URDU_DIGITS, URDU_PUNCTUATIONS, URDU_DIACRITICS
 from urdu_characters import URDU_ALL_CHARACTERS_UNICODE
-import re
-
-urdu_unicode_range = re.compile("[^\u0600-\u06ff]+")
 
 
-class TestUrduAlphabet(object):
+URDU_UNICODE_RANGE = re.compile("[^\u0600-\u06ff]+")
+
+
+class TestUrduAlphabet():
     """Test cases for Urdu Alphabet"""
 
     def test_urdu_alphabet(self):
         """ Test urdu_alphabet"""
         for character in URDU_ALPHABETS:
             assert character in URDU_ALL_CHARACTERS
-            assert len(urdu_unicode_range.findall(character)) == 0
+            assert len(URDU_UNICODE_RANGE.findall(character)) == 0
 
     def test_urdu_digits(self):
         """ Test """
         for character in URDU_DIGITS:
             assert character in URDU_ALL_CHARACTERS
-            assert len(urdu_unicode_range.findall(character)) == 0
+            assert len(URDU_UNICODE_RANGE.findall(character)) == 0
 
     def test_urdu_punctuation(self):
         """ Test """
         for character in URDU_PUNCTUATIONS:
             assert character in URDU_ALL_CHARACTERS
-            assert len(urdu_unicode_range.findall(character)) == 0
+            assert len(URDU_UNICODE_RANGE.findall(character)) == 0
 
     def test_diacritics(self):
         """ Test """
         for character in URDU_DIACRITICS:
             assert character in URDU_ALL_CHARACTERS
-            assert len(urdu_unicode_range.findall(character)) == 0
+            assert len(URDU_UNICODE_RANGE.findall(character)) == 0
 
     def test_unicode(self):
         """ Test """
